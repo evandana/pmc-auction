2bc2fb9907e9207fe72806bf72ef2a34a07f7029
Evan Dana <edana@sapient.com>
2016-04-09 10:46:38 -0400

    updating auction item details layout

--- src/components/auctionList/auctionItemDetail.js
+++ src/components/auctionList/auctionItemDetail.js
@@ -21,16 +21,14 @@ class AuctionItemDetail extends Component {
 		return (
 			<div className="detail-row-l"  onClick={ e => toggleAuctionDetail(this.props.data.id, e) } >
 				<div colSpan="4">
-					<h3>{data.description}</h3>
+					<h3>{data.title}</h3>
+					<p>{data.description}</p>
 					<div className="detail-field"><label>closeDate</label><span>{data.closeDate}</span></div>
 					<div className="detail-field"><label>description</label><span>{data.description}</span></div>
-					<div className="detail-field"><label>donorId</label><span>{data.donorId}</span></div>
 					<div className="detail-field"><label>donorName</label><span>{data.donorName}</span></div>
 					<div className="detail-field"><label>expiration</label><span>{data.expiration}</span></div>
-					<div className="detail-field"><label>id</label><span>{data.id}</span></div>
 					<div className="detail-field"><label>openDate</label><span>{data.openDate}</span></div>
 					<div className="detail-field"><label>openingBid</label><span>{data.openingBid}</span></div>
-					<div className="detail-field"><label>title</label><span>{data.title}</span></div>
 					<div className="detail-row-image"><img src={urlStr} /></div>
 				</div>
 			</div>
