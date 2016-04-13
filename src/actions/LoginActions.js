fcfecd378d49dc65a663b309863bebac426455d5
David Thomas <david.thomas3@gmail.com>
2016-04-13 14:26:17 -0400

    cleaning up after myself

--- src/actions/LoginActions.js
+++ src/actions/LoginActions.js
@@ -3,20 +3,11 @@ import { hashHistory } from 'react-router'
 import firebase from '../utils/firebaseAdapter'
 
 export const LOGIN_CONSTANTS = {
-    // AUTH_CHECK_ERROR: 'AUTH_CHECK_ERROR',
-    // AUTH_CHECK_SUCCESS: 'AUTH_CHECK_SUCCESS',
     AUTH_CHECK_REQUEST: 'AUTH_CHECK_REQUEST',
     AUTH_CHECK_RESPONSE: 'AUTH_CHECK_RESPONSE',
     AUTH_SUCCESS: 'AUTH_SUCCESS',
     AUTH_FAIL: 'AUTH_FAIL',
     SET_USER: 'SET_USER'
-    // LOGIN_GOOGLE: 'LOGIN_GOOGLE',
-    // LOGIN_GOOGLE_ERROR: 'LOGIN_GOOGLE_ERROR',
-    // LOGIN_GOOGLE_SUCCESS: 'LOGIN_GOOGLE_SUCCESS',
-    // LOGOUT_USER: 'LOGOUT_USER',
-    // REQUEST_AUTH: 'REQUEST_AUTH',
-    // REQUEST_LOGIN_GOOGLE: 'REQUEST_LOGIN_GOOGLE',
-    // REQUEST_ROUTE_CHANGE: 'REQUEST_ROUTE_CHANGE'
 }
 
 export const LoginActions = {
@@ -32,14 +23,14 @@ export const LoginActions = {
     authCheckResponse(user) {
         console.log('auth response called ', user)
         if (user) {
-            return dispatch => { dispatch(this.findUser(user)) }
+            return dispatch => { dispatch(this.findAndSetUser(user)) }
         } else {
             return { type: LOGIN_CONSTANTS.AUTH_FAIL }
         }
 
     },
     
-    findUser (authData) {
+    findAndSetUser (authData) {
         console.log('setting up findUser');
 
         return dispatch => {
@@ -67,23 +58,6 @@ export const LoginActions = {
             user
         }
     },
-    
-    authCheckSuccessObj(user) {
-        return {
-            type: LOGIN_CONSTANTS.AUTH_CHECK_SUCCESS,
-            user
-        }
-    },
-
-    requestRouteChange(route, store) {
-
-        let user = store.getState().login;
-        // console.log('requestRouteChange', route, user, !!user);
-
-        if (route.pathname !== '/login' && !user) {
-            hashHistory.push('/login');
-        }
-    },
 
     requestLoginGoogle(user) {
         // will never call success callback, only error callback
@@ -97,45 +71,6 @@ export const LoginActions = {
         }
     }
 }
-
-// -------------------------------------------------------
-
-// // firebase read/write adapter
-// import firebase from 'utils/firebaseAdapter'
-
-// export const FETCH_AUCTIONS = 'FETCH_AUCTIONS'
-// export const LOAD_AUCTION = 'LOAD_AUCTION'
-// export const PLACE_BID = 'PLACE_BID'
-// export const TOGGLE_AUCTION_DETAIL = 'TOGGLE_AUCTION_DETAIL'
-
-// export function fetchAuctions() {
-//     return dispatch => {
-//         firebase.loadAuctions( auction => dispatch(loadAuctionObj(auction)) )
-//     }
-// }
-
-// export function loadAuctionObj(auction) {
-//     return {
-//         type: LOAD_AUCTION,
-//         auction
-//     }
-// }
-
-// export function placeBid(auctionId, bidAmount) {
-//     return {
-//         type: PLACE_BID,
-//         auctionId,
-//         bidAmount
-//     }
-// }
-
-// export function toggleAuctionDetail(auctionId) {
-//     return {
-//         type: TOGGLE_AUCTION_DETAIL,
-//         auctionId
-//     }
-// }
-
 
 function getUidFromAuth (authData) {
 
