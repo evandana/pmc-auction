945d9f5e7c1779a01a586f5b4da77c4ed02ebc06
Evan Dana <edana@sapient.com>
2016-04-17 13:45:12 -0400

    tweaking footer

--- src/components/containers/login/Login.js
+++ src/components/containers/login/Login.js
@@ -20,11 +20,14 @@ const Login = React.createClass({
             <div className="pure-form pure-form-aligned">
                 <fieldset>
                     <div className="pure-controls">
+                        <h1>Welcome to the Happiness Exchange</h1>
+                        <p>Login with a Google account to view the auctions!</p>
                         <button
                             className="pure-button pure-button-primary"
                             onClick={this.requestLoginGoogle}>
                             Google Login
                         </button>
+                        <p>RSVP to the <a href="https://www.facebook.com/events/198978703805744/">event</a></p>
                     </div>
                 </fieldset>
             </div>
