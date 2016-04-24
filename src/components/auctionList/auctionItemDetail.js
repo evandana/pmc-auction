e29832264a7c9139d7b48a14b0059e964e6b2b49
Evan Dana <edana@sapient.com>
2016-04-23 22:49:17 -0400

    bid incrementing works, until new bid is submitted

--- src/components/auctionList/auctionItemDetail.js
+++ src/components/auctionList/auctionItemDetail.js
@@ -5,17 +5,18 @@ import { connect } from 'react-redux'
 import { placeBid } from '../../actions/AuctionActions'
 
 // Material UI
+import Avatar from 'material-ui/lib/avatar';
 import Card from 'material-ui/lib/card/card';
 import CardActions from 'material-ui/lib/card/card-actions';
 import CardHeader from 'material-ui/lib/card/card-header';
 import CardMedia from 'material-ui/lib/card/card-media';
-import CardTitle from 'material-ui/lib/card/card-title';
-import FlatButton from 'material-ui/lib/flat-button';
 import CardText from 'material-ui/lib/card/card-text';
-import Avatar from 'material-ui/lib/avatar';
-import FloatingActionButton from 'material-ui/lib/floating-action-button';
-import ContentClear from 'material-ui/lib/svg-icons/content/clear';
+import CardTitle from 'material-ui/lib/card/card-title';
 import Colors, { cyan200 } from 'material-ui/lib/styles/colors';
+import ContentClear from 'material-ui/lib/svg-icons/content/clear';
+import Dialog from 'material-ui/lib/dialog';
+import FlatButton from 'material-ui/lib/flat-button';
+import FloatingActionButton from 'material-ui/lib/floating-action-button';
 
 import { getImageForEnv } from '../../images/index'
 
@@ -27,22 +28,107 @@ class AuctionItemDetail extends Component {
 
     constructor(props) {
         super(props)
-  	}
-
-	render() {
-		const { placeBid, toggleAuctionDetail } = this.props
-
 
 		let user = this.props.user,
 			data = this.props.auctions.find(
 					auction => { return auction.id === this.props.data.id; }
 				);
 
-		//debugger;
+		let highestBid = {bidAmount: 0};
+		for (let bid in data.bids) {
+			if (parseInt(data.bids[bid].bidAmount, 10) > highestBid.bidAmount) {
+				highestBid = data.bids[bid];
+			}
+		}
+
+		this.increment = parseInt(data.incrementAmount, 10);
+		this.bidAmountMin = parseInt(highestBid.bidAmount, 10) + this.increment;
+
+	    this.state = {
+	      open: false,
+	      bidDisplayAmount: data.bids ? parseInt(highestBid.bidAmount, 10) + this.increment : data.openingBid,
+	      highestBid: highestBid,
+	      data: data,
+	      user: user
+	    };
+  	}
+
+	handleOpen = () => {
+		this.setState({open: true});
+	}
+
+	handleClose = () => {
+		this.setState({open: false});
+	}
+
+	increaseBidAmount() {
+		const bidDisplayAmount = this.state.bidDisplayAmount + this.increment;
+		this.setState({bidDisplayAmount: bidDisplayAmount});
+		console.log('bidDisplayAmount', bidDisplayAmount);
+	}
+	decreaseBidAmount() {
+		const bidDisplayAmount = this.state.bidDisplayAmount - this.increment >= this.bidAmountMin
+			?
+				this.state.bidDisplayAmount - this.increment
+			:
+				this.bidAmountMin;
+		this.setState({bidDisplayAmount: bidDisplayAmount});
+		console.log('bidDisplayAmount', bidDisplayAmount);
+	}
+
+	render() {
+
+
+
+
+
+
+
+
+
+
+		// TODO: update min bid amount on state update from bottom of this doc
+
+
+
+
+
+
+
+
+
+
+		let data = this.state.data,
+			user = this.state.user;
+
+		// modal actions
+	    const actions = [
+	      <FlatButton
+	        label="Cancel"
+	        secondary={true}
+	        onTouchTap={this.handleClose}
+	      />,
+	      <FlatButton
+	        label="Submit"
+	        primary={true}
+	        keyboardFocused={true}
+	        onTouchTap={ e => {
+	        	this.handleClose();
+	        	placeBid({
+					bidderObj: user,
+					bidAmount: this.state.bidDisplayAmount,
+					auctionId: data.id
+				}, e)
+	        }}
+	        // onTouchTap={this.handleClose}
+	      />,
+	    ];
+
+		const { placeBid, toggleAuctionDetail } = this.props
+
+		// debugger;
 
 		let urlStr = getImageForEnv( 'auction-big/' + data.image + '.png');
-
-		// console.log('item detail props', this.props, this.props.data.id)
 
 		let style = {
 			detailsPage: {
@@ -71,8 +157,8 @@ class AuctionItemDetail extends Component {
 		// <p>{data.description}</p>
 
 		// console.log('auctionItemDetails', this.props)
-		console.log('data', data);
-		console.log('data.bids', data.bids);
+		// console.log('data', data);
+		// console.log('data.bids', data.bids);
 
 		return (
 			<div className="auction-item-detail">
@@ -82,7 +168,7 @@ class AuctionItemDetail extends Component {
 					    	backgroundColor={cyan200}
 					    	mini={true}
 					    	style={style.closeButton}
-					    	onClick={ e => toggleAuctionDetail(this.props.data.id, e) }
+					    	onClick={ e => toggleAuctionDetail(data.id, e) }
 					      >
 					      <ContentClear />
 					    </FloatingActionButton>
@@ -94,18 +180,25 @@ class AuctionItemDetail extends Component {
 					{
 						this.props.config.BIDDING_OPEN
 						?
-							<CardActions>
-								<FlatButton label="-" />
-								<FlatButton
-									label={ 'bid $' + (parseInt(data.openingBid, 10) + 5) }
-									onClick={ e => placeBid({
-										bidderObj: user,
-										bidAmount: data.openingBid,
-										auctionId: data.id
-									}, e) }
-								/>
-								<FlatButton label="+" />
-							</CardActions>
+							<div>
+								<CardActions>
+									<FlatButton label="-" onTouchTap={() => this.decreaseBidAmount()}/>
+									<FlatButton
+										label={ 'bid $' + this.state.bidDisplayAmount }
+										onTouchTap={this.handleOpen}
+									/>
+									<FlatButton label="+" onTouchTap={() => this.increaseBidAmount()}/>
+								</CardActions>
+								<Dialog
+									title={'Confirm Bid for $' + this.state.bidDisplayAmount}
+									actions={actions}
+									modal={false}
+									open={this.state.open}
+									onRequestClose={this.handleClose}
+									>
+									{data.title} with {data.donorName}
+								</Dialog>
+							</div>
 						:
 							<CardText style={style.biddingNotAvailable}>Bidding closed at this time</CardText>
 					}
