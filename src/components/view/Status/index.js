538fce5cbf6872f1856357ad72b7fb778dde1b34
Evan Dana <evan.dana@gmail.com>
2018-01-27 12:48:10 -0500

    Owned auction table on status page (no actions or true status yet)

--- src/components/view/Status/index.js
+++ src/components/view/Status/index.js
@@ -7,29 +7,31 @@ import {
     TableRow,
     TableRowColumn,
 } from 'material-ui/Table';
+import FlatButton from 'material-ui/FlatButton';
 
 const Status = (props) => {
 
-    const { auctionsWithUserBids, ...rest } = props;
-
-    const leadingBidTable = createLeadingBidTable(auctionsWithUserBids);
+    const { user, config, auctionsWithUserBids, auctionsOwned, ...rest } = props;
 
     return (
         <div className='page'>
             <div className='text-content'>
                 <h1>Status</h1>
 
-                <h2>Auctions with Your Bids</h2>
-                <p>If you are the top bidder when the auction closes, you are expected to claim that item.</p>
-                <p>If an auction owner is awarding multiple winners and you have one of the highest few bids, you may be offered a chance to accept this at your highest bid for that item.</p>
-                {leadingBidTable}
+                <section>
+                    <h2>Auctions with Your Bids</h2>
+                    <p>If you are the top bidder when the auction closes, you are expected to claim that item.</p>
+                    <p>If an auction owner is awarding multiple winners and you have one of the highest few bids, you may be offered a chance to accept this at your highest bid for that item.</p>
+                    {createLeadingBidTable(auctionsWithUserBids, config.BIDDING_OPEN)}
+                </section>
 
-                <h2>Owned Auctions</h2>
-                <pre>[tables with data for each owned auction]</pre>
-
-                <h2>Confirmed Won Auctions</h2>
-                <p>Congrats have been confirmed to have won these auctions!</p>
-                <pre>[tables with data for each won auction including contact information]</pre>
+                {!user.permissions.donor || !auctionsOwned ? '' : (
+                    <section>
+                        <h2>Owned Auctions</h2>
+                        <p>If you are the top bidder when the auction closes, you are expected to claim that item.</p>
+                        {createOwnedAuctionTable(auctionsOwned, config.BIDDING_OPEN)}
+                    </section>
+                )}
 
             </div>
         </div>
@@ -38,35 +40,92 @@ const Status = (props) => {
 
 export default Status;
 
-function createLeadingBidTable(auctionsWithUserBids) {
+
+
+function createLeadingBidTable(auctionsWithUserBids, biddingOpen) {
 
     return (
         <Table selectable={false} >
             <TableHeader
                 displaySelectAll={false}
                 adjustForCheckbox={false}
-                >
+            >
                 <TableRow>
                     <TableHeaderColumn colSpan={3}>Title</TableHeaderColumn>
+                    <TableHeaderColumn colSpan={2}>Status</TableHeaderColumn>
                     <TableHeaderColumn>Owner</TableHeaderColumn>
                     <TableHeaderColumn>Highest Bid</TableHeaderColumn>
                     <TableHeaderColumn>Your Bid</TableHeaderColumn>
                     <TableHeaderColumn>Bid Rank</TableHeaderColumn>
-                    <TableHeaderColumn>Auction Items Offered</TableHeaderColumn>
+                    <TableHeaderColumn># Offered</TableHeaderColumn>
                 </TableRow>
             </TableHeader>
             <TableBody
                 displayRowCheckbox={false}
-                >
+            >
                 {auctionsWithUserBids.map(auctionWithUserBid => {
                     return (
                         <TableRow selectable={false} key={auctionWithUserBid.uid}>
                             <TableRowColumn colSpan={3}>{auctionWithUserBid.title}</TableRowColumn>
-                            <TableRowColumn>{auctionWithUserBid.owner}</TableRowColumn>
+                            <TableRowColumn colSpan={2}>Bidding Open | Won | Not Won | <FlatButton>Accept for $100</FlatButton> | Pending Owner</TableRowColumn>
+                            <TableRowColumn>{auctionWithUserBid.owner.displayName}</TableRowColumn>
                             <TableRowColumn>${auctionWithUserBid.highBid}</TableRowColumn>
                             <TableRowColumn>${auctionWithUserBid.userHighBidValue}</TableRowColumn>
                             <TableRowColumn>{auctionWithUserBid.userHighBidRank} / {auctionWithUserBid.bidCount}</TableRowColumn>
                             <TableRowColumn>{auctionWithUserBid.countOffered}</TableRowColumn>
+                        </TableRow>
+                    );
+                })}
+            </TableBody>
+        </Table>
+    );
+}
+
+
+function createOwnedAuctionTable(auctionsOwned, biddingOpen) {
+
+    const getBidStatus = (bid, index) => {
+        if (!bid.ownerConfirmed) {
+            return <FlatButton primary={index===0}>Confirm</FlatButton>
+        } else if (!bid.bidderConfirmed) {
+            return 'Pending Bidder';
+        } else if (bid.ownerConfirmed && bid.bidderConfirmed) {
+            return 'Confirmed';
+        }
+    }
+
+    return (
+        <Table selectable={false} >
+            <TableHeader displaySelectAll={false} adjustForCheckbox={false} >
+                <TableRow>
+                    <TableHeaderColumn colSpan={3}>Title</TableHeaderColumn>
+                    <TableHeaderColumn>Bids</TableHeaderColumn>
+                </TableRow>
+            </TableHeader>
+            <TableBody displayRowCheckbox={false}>
+                {auctionsOwned.map(auctionOwned => {
+                    return (
+                        <TableRow selectable={false} key={auctionOwned.uid}>
+                            <TableRowColumn>{auctionOwned.title}</TableRowColumn>
+                            <TableRowColumn>{!auctionOwned.topBids || !auctionOwned.topBids.length ? 'No bids yet' : (
+
+                                <Table selectable={false}>
+                                    <TableBody displayRowCheckbox={false}>
+                                        {auctionOwned.topBids.map((bid, index) => {
+                                            return (
+                                                <TableRow selectable={false} key={index}>
+                                                    <TableRowColumn>${bid.bidAmount}</TableRowColumn>
+                                                    <TableRowColumn>{bid.bidderObj.name}</TableRowColumn>
+                                                    {biddingOpen ? '' : (
+                                                        <TableRowColumn>{getBidStatus(bid, index)}</TableRowColumn>
+                                                    )}
+                                                </TableRow>
+                                            );
+                                        })}
+                                    </TableBody>
+                                </Table>
+                            
+                            )}</TableRowColumn>
                         </TableRow>
                     );
                 })}
