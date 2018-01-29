db222bae42c40bf3bc4eb2d4086f0d5eb299e21b
Evan Dana <evan.dana@gmail.com>
2018-01-28 21:49:11 -0500

    Status page responsiveness

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 /** MATERIAL UI **/
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import {cyan50,limeA200,yellow800} from 'material-ui/styles/colors';
+import {cyan50,cyan900,limeA200,yellow800} from 'material-ui/styles/colors';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 /** ROUTER **/
 import { Route } from 'react-router-dom';
@@ -75,6 +75,7 @@ class App extends Component {
                 accent1Color: limeA200,
                 ternaryTextColor: yellow800,
                 fadedPrimary1Color: cyan50,
+                primary2Color: cyan900,
                 // DEFAULTS
                 // primary1Color: cyan500,
                 // primary2Color: cyan700,
