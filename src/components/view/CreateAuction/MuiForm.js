3d4e1233f56395ee082fa9fc5f8ac3dbc9d719da
Evan Dana <evan.dana@gmail.com>
2018-02-18 16:15:02 -0500

    Edit auction form somewhat working

--- src/components/view/CreateAuction/MuiForm.js
+++ src/components/view/CreateAuction/MuiForm.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
 import TextField from 'material-ui/TextField'
 import FlatButton from 'material-ui/FlatButton'
 import RaisedButton from 'material-ui/RaisedButton'
+// import muiThemeable from 'material-ui/styles/muiThemeable';
 // import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton'
 import Checkbox from 'material-ui/Checkbox'
 // import SelectField from 'material-ui/SelectField'
@@ -40,11 +42,10 @@ const renderTextField = ({
   ...custom
 }) => (
     <TextField
-      hintText={label}
       floatingLabelText={label}
       errorText={touched && error}
-      floatingLabelFixed={type==='date' ? true : false}
-      hintText={type==='date' ? '' : label}
+      floatingLabelFixed={type === 'date' ? true : false}
+      hintText={type === 'date' ? '' : label}
       type={type}
       {...input}
       {...custom}
@@ -59,7 +60,18 @@ const renderCheckbox = ({ input, label }) => (
   />
 )
 
-{
+class MuiForm extends Component {
+
+  constructor(props) {
+    super(props);
+    // this.themePalette = this.props.muiTheme.palette;
+    this.state = {
+      initialValues: this.props.initialValues
+    };
+  }
+
+
+
   // const renderRadioGroup = ({ input, ...rest }) => (
   //   <RadioButtonGroup
   //     {...input}
@@ -68,7 +80,7 @@ const renderCheckbox = ({ input, label }) => (
   //     onChange={(event, value) => input.onChange(value)}
   //   />
   // )
-  
+
   // const renderSelectField = ({
   //   input,
   //   label,
@@ -85,82 +97,122 @@ const renderCheckbox = ({ input, label }) => (
   //       {...custom}
   //     />
   //   )
-}
 
-const MaterialUiForm = props => {
-  const { handleSubmit, submitForm, pristine, reset, submitting } = props
-  return (
-    <form onSubmit={handleSubmit(submitForm)}>
 
-      <section className="row middle-xs middle-sm">
-        <RaisedButton className="col-xs-6" type="submit" style={{padding:0}} primary disabled={pristine || submitting}>
-          Submit
-        </RaisedButton>
-        <FlatButton className="col-xs-6" type="button" style={{padding:0}} disabled={pristine || submitting} onClick={reset}>
-          Clear Values
-        </FlatButton>
-      </section>
+  render() {
 
-      <section classname="row">
-        <Field
-          name="title"
-          type="text"
-          component={renderTextField}
-          label="Title"
-        />
-        <Field
-          name="subTitle"
-          type="text"
-          component={renderTextField}
-          label="Subtitle"
-        />
-        <Field
-          name="location"
-          type="text"
-          component={renderTextField}
-          label="Location"
-        />
-        <Field
-          name="useBy"
-          type="date"
-          component={renderTextField}
-          label="Use By"
-        />
-        <Field
-          name="openingBid"
-          type="number"
-          component={renderTextField}
-          label="Opening Bid"
-        />
-        <Field
-          name="bidIncrement"
-          type="number"
-          component={renderTextField}
-          label="Bid Increment"
-        />
-        <Field
-          name="numberOffered"
-          type="number"
-          component={renderTextField}
-          label="Number Offered"
-        />
-        <Field
-          name="description"
-          type="text"
-          component={renderTextField}
-          label="Description"
-          multiLine={true}
-          rows={2}
-        />
-        <Field name="featured" component={renderCheckbox} label="Featured" />
-        <Field name="show" component={renderCheckbox} label="Show" />
-      </section>
-    </form>
-  )
+    const { handleSubmit, createAuctionSubmitForm, pristine, reset, submitting, initialValues } = this.props;
+
+    // this.setState({initialValues});  
+
+    const style = {
+      field: {
+        borderRight: '.5em solid #fff'
+      }
+    };
+
+    return (
+      <form
+        onSubmit={handleSubmit(createAuctionSubmitForm)}
+      >
+
+        <section className="row middle-xs middle-sm">
+          <RaisedButton className="col-xs-8" type="submit" style={{ padding: 0 }} primary disabled={pristine || submitting}>
+            Submit
+          </RaisedButton>
+          <span className="col-xs-1"> </span>
+          <RaisedButton className="col-xs-3" type="button" style={{ padding: 0 }} disabled={pristine || submitting} onClick={reset}>
+            Clear Values
+          </RaisedButton>
+        </section>
+
+        <section className="row">
+          <Field
+            className="col-xs-12"
+            style={style.field}
+            name="title"
+            type="text"
+            component={renderTextField}
+            label="Title"
+          />
+          <Field
+            className="col-xs-6"
+            style={style.field}
+            name="subTitle"
+            type="text"
+            component={renderTextField}
+            label="Subtitle"
+          />
+          <Field
+            className="col-xs-6"
+            style={style.field}
+            name="location"
+            type="text"
+            component={renderTextField}
+            label="Location"
+          />
+          <Field
+            className="col-xs-6"
+            style={style.field}
+            name="useBy"
+            type="date"
+            component={renderTextField}
+            label="Use By"
+          />
+          <Field
+            className="col-xs-6"
+            style={style.field}
+            name="openingBid"
+            type="number"
+            component={renderTextField}
+            hintText={10}
+            label="Opening Bid"
+          />
+          <Field
+            className="col-xs-6"
+            style={style.field}
+            name="bidIncrement"
+            type="number"
+            component={renderTextField}
+            hintText={5}
+            label="Bid Increment"
+          />
+          <Field
+            className="col-xs-6"
+            style={style.field}
+            name="numberOffered"
+            type="number"
+            component={renderTextField}
+            label="Qty Offered"
+          />
+          <Field
+            className="col-xs-12"
+            style={style.field}
+            name="description"
+            type="text"
+            component={renderTextField}
+            label="Description"
+            multiLine={true}
+            rows={2}
+          />
+          <Field
+            className="col-xs-6"
+            name="featured"
+            component={renderCheckbox}
+            label="Featured"
+          />
+          <Field
+            className="col-xs-6"
+            name="show"
+            component={renderCheckbox}
+            label="Show"
+          />
+        </section>
+      </form>
+    )
+  }
 }
 
 export default reduxForm({
-  form: 'MaterialUiForm', // a unique identifier for this form
   validate,
-  // asyncValidate
-})(MaterialUiForm)
+})(MuiForm)
