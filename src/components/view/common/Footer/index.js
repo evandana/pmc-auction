83e72fcfb4f430452636d57661e730774288bf5c
Evan Dana <evan.dana@gmail.com>
2018-03-15 23:09:04 -0400

    Ensuring Footer is on z-index top

--- src/components/view/common/Footer/index.js
+++ src/components/view/common/Footer/index.js
@@ -4,7 +4,7 @@ import './footer.css'
 const Footer = () => {
 
     return (
-        <footer>
+        <footer style={{zIndex:100}}>
             100% of every dollar goes to cancer research at DFCI.
         </footer>
     )
