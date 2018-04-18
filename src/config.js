5afc3d55d3e232f648ef8f1cc23a854aa49aded7
Evan Dana <evan.dana@gmail.com>
2018-04-18 09:52:16 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const firebase = {
-  apiKey: "AIzaSyA3JKgI5y36_yDZUcL2JTFWaKKziF6ikgg",
-  authDomain: "pmc-2018.firebaseapp.com",
-  databaseURL: "https://pmc-2018.firebaseio.com",
-  projectId: "pmc-2018",
-  storageBucket: "pmc-2018.appspot.com",
-  messagingSenderId: "606291690097"
-};
+  apiKey: "AIzaSyCLpLuWKToIEUu5oSv_NpmsEYtEkZhd81c",
+  authDomain: "pmc-2018-demo.firebaseapp.com",
+  databaseURL: "https://pmc-2018-demo.firebaseio.com",
+  projectId: "pmc-2018-demo",
+  storageBucket: "pmc-2018-demo.appspot.com",
+  messagingSenderId: "346886877359"
+}
 
 export default {
   firebase
